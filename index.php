498b6d73c73f3937ed4f8586e4585b142da0100b
HASKADOG <cplasplas16@gmail.com>
2019-04-23 19:49:47 +0300

    Header done

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Главная</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/adaptive.css">
+</head>
+<body>
+	<section class="first">
+		<div class="header-main header">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4">
+						<div class="header-menu h-100 d-flex flex-row align-items-center justify-content-between">
+							<div class="header-menu__inner"><a href="#">о нас</a></div>
+							<div class="header-menu__inner"><a href="#">калькулятор</a></div>
+							<div class="header-menu__inner"><a href="#">контакты</a></div>
+						</div>
+					</div>
+					<div class="col-lg-3">
+						<div class="hader__logo d-flex justify-content-center "><img src="img/logo.png" alt="Логотип"></div>
+					</div>
+					<div class="col-lg-5  ">
+						<div class="header__right-menu d-flex flex-row  justify-content-between h-100 align-items-center ">
+							<div class="header__icons-wrapper  w-25  d-flex flex-row justify-content-around">
+								<a class="header__icons" href="#"><img src="img/ico-user.png" alt=""></a>
+								<a class="header__icons" href="#"><img src="img/ico-question.png" alt=""></a>
+							</div>
+							
+							<div class="header__lang-choose  d-flex flex-row">
+								<div class="rus lang-active">рус</div>
+								<div class="eng">eng</div>
+							</div>
+							<div class="call-back d-flex flex-row justify-content-around align-items-center">
+								<img src="img/ico-phone.png" alt="">
+								<div class="call-back-text"> Заказть звонок</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
